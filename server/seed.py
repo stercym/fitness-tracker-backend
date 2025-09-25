d00fc74c42846809a7d4d68a7d619116ed91df65
samuel nyota karanja <samuel.karanja3@student.moringaschool.com>
2025-09-25 15:30:41 +0300

    modify seed.py

--- server/seed.py
+++ server/seed.py
@@ -9,25 +9,22 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
-    # ---------------- USERS ----------------
     user1 = User(username="samuel")
     user2 = User(username="jane")
 
     db.session.add_all([user1, user2])
     db.session.commit()
 
-    # ---------------- GOALS ----------------
+
     goal1 = Goal(name="Strength Training")
     goal2 = Goal(name="Cardio Endurance")
 
-    # Link goals to users
     user1.goals.extend([goal1, goal2])
     user2.goals.append(goal1)
 
     db.session.add_all([goal1, goal2])
     db.session.commit()
 
-    # ---------------- WORKOUTS ----------------
     workout1 = Workout(
         title="Leg Day",
         date="2025-09-20",
@@ -50,7 +47,7 @@ with app.app_context():
     db.session.add_all([workout1, workout2, workout3])
     db.session.commit()
 
-    # ---------------- EXERCISES ----------------
+
     squat = Exercise(exercise_name="Squat", goal_id=goal1.id)
     bench = Exercise(exercise_name="Bench Press", goal_id=goal1.id)
     run = Exercise(exercise_name="Running", goal_id=goal2.id)
@@ -58,7 +55,7 @@ with app.app_context():
     db.session.add_all([squat, bench, run])
     db.session.commit()
 
-    # ---------------- EXERCISE LOGS ----------------
+ 
     log1 = ExerciseLog(sets=4, reps=10, weight=80, workout_id=workout1.id, exercise_id=squat.id)
     log2 = ExerciseLog(sets=3, reps=8, weight=60, workout_id=workout3.id, exercise_id=bench.id)
     log3 = ExerciseLog(sets=1, reps=1, weight=None, workout_id=workout2.id, exercise_id=run.id)
@@ -66,4 +63,4 @@ with app.app_context():
     db.session.add_all([log1, log2, log3])
     db.session.commit()
 
-    print("Database seeded successfully! ✅")
+    print("Database seeded successfully!")
